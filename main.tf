6d87a0460115252cc4d9d7dad3219dc6cc9ddc61
adamjpadilla <adamjpadilla@outlook.com>
2026-05-20 09:59:01 -0400

    Added security group

--- main.tf
+++ main.tf
@@ -5,10 +5,26 @@ provider "aws" {
 
 
 resource "aws_instance" "example" {
-    ami           = "ami-00a9f44477dd83e3d"
-    instance_type = "t3.micro"
+    ami           = "ami-0fe18bc3cfa53a248"
+    instance_type = "t2.micro"
 
-    tags = {
-        Name = "Terraform-example"
-    }
+user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World!" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+tags = {
+    Name = "terraform-example"
+  }
 } 
+
+resource "aws_security_group" "instance" {
+  name        = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  } 
+}
