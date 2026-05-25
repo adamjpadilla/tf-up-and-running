43eeb50f2175a6762f8e9a85d239d01ca3a5d73f
adamjpadilla <adamjpadilla@outlook.com>
2026-05-25 18:15:24 -0400

    add chapter 3 main.tf

---
+++ chapter-3/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-up-and-running"
+
+#prevent accidental deletion of this S3 bucket
+lifecycle {
+  prevent_destroy = true
+  }
+}
+
